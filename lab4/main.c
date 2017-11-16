128d06dc3af27b273b248f0343a32dbc2f8bc6d4
Srisri Gokanapudy <sgoka001@ucr.edu>
2017-11-16 15:34:52 -0800

    added menu

--- lab4/main.c
+++ lab4/main.c
@@ -31,21 +31,23 @@ unsigned char temp = 0; //loc of stepper motor atm
 unsigned long numPhases;
 unsigned char cnt = 0;
 
+unsigned char menu_key;
 unsigned char key; //first entry on keypad
 unsigned char key2; //second entry on keypad
-unsigned char bluetooth_data; //first entry on phone
-unsigned char bluetooth_data2; //second entry on phone
 unsigned char bt_check = 0;
 
+unsigned char menu_check = 0;
 unsigned char checker = 0; // 1 = valid entry, 0 = no key entered
 unsigned char checker2 = 0; // 1 = valid entry, 0 = no key entered
 
-unsigned char lr = 0; // unlock = 2
+unsigned char password = '1';
+unsigned char password2 = '2';
 
-unsigned char room = 0;
+unsigned char lr = 0; // unlock = 2
+unsigned char room = 0; // bed = 1 garage = 2
 //////////////////////////////////////////////////////////////////////// LOCAL VARIABLES ////////////////////////////////////////////////////////////////////////
 
-enum MotorState {READ_MOTION, READ_KEY_INIT, READ_KEY_WAIT, READ_KEY_INIT_2, READ_KEY, LOCK, UNLOCK} motor_state;
+enum MotorState {READ_MOTION, MENU, READ_KEY_WAIT, READ_KEY_INIT, CURRENT_PASS, CHANGE_PASS, READ_KEY_WAIT_2, READ_KEY_INIT_2, READ_KEY, LOCK, UNLOCK} motor_state;
 
 void Motor_Init(){
 	motor_state = READ_MOTION;
@@ -53,24 +55,45 @@ void Motor_Init(){
 
 void Motor_Tick()
 {
-	
 	char garage_button = (GetBit(~PIND, 3)); //if button is pressed then = 8
 	char motion_sensor = (GetBit(~PIND, 2)); //if motion is sensed then = 4
 	switch(motor_state)
 	{	
 		case READ_MOTION:
-			if(motion_sensor == 4){motor_state = READ_KEY_INIT;}
+			if(motion_sensor == 4){motor_state = MENU;}
 			else if(motion_sensor == 0){motor_state = READ_MOTION;}
 			break;
 		
-		case READ_KEY_INIT:
-			if(checker == 1){motor_state = READ_KEY_WAIT;}
-			else if (checker == 0){motor_state = READ_KEY_INIT;}
+		case MENU:
+			if(menu_check == 1){motor_state = READ_KEY_WAIT;}
+			else if(menu_check == 2){motor_state = CHANGE_PASS;}
+			else if(menu_check == 3){motor_state = CURRENT_PASS;}
+			else if(menu_check == 0){motor_state = MENU;}
 			break;
 			
 		case READ_KEY_WAIT:
-			if(cnt > 200){motor_state = READ_KEY_INIT_2; cnt = 0;}
+			if(cnt > 100){motor_state = READ_KEY_INIT; cnt = 0;}
 			else{motor_state = READ_KEY_WAIT; cnt = cnt + 1;}
+			break;
+			
+		case READ_KEY_INIT:
+			if(checker == 1){motor_state = READ_KEY_WAIT_2;}
+			else if (checker == 0){motor_state = READ_KEY_INIT;}
+			break;
+			
+		case CURRENT_PASS:
+			if(cnt > 100){motor_state = MENU; cnt = 0;}
+			else{motor_state = CURRENT_PASS; cnt = cnt + 1;}
+			break;
+			
+		case CHANGE_PASS:
+			if(cnt > 100){motor_state = MENU; cnt = 0;}
+			else{motor_state = CHANGE_PASS; cnt = cnt + 1;}
+			break;
+			
+		case READ_KEY_WAIT_2:
+			if(cnt > 200){motor_state = READ_KEY_INIT_2; cnt = 0;}
+			else{motor_state = READ_KEY_WAIT_2; cnt = cnt + 1;}
 			break;
 			
 		case READ_KEY_INIT_2:
@@ -109,11 +132,39 @@ void Motor_Tick()
 				nokia_lcd_write_string("No", 2);
 				nokia_lcd_set_cursor(15, 20);
 				nokia_lcd_write_string("Motion", 2);
+				//nokia_lcd_set_cursor(0, 40);
 				//nokia_lcd_write_char(motion_sensor + '0', 1);
 				//nokia_lcd_write_char(garage_button + '0', 1);
 			}
 			nokia_lcd_render();
 			break;
+			
+		case MENU:
+			menu_key = GetKeypadKey();
+			if ((menu_key == 'A')){
+				menu_check = 1;
+			}
+			else if ((menu_key == 'B')){
+				menu_check = 2;
+			}
+			else if ((menu_key == 'C')){
+				menu_check = 3;
+			}
+			else{
+				menu_check = 0;
+			}
+			nokia_lcd_clear();
+			nokia_lcd_set_cursor(0, 10);
+			nokia_lcd_write_string("A-enter code", 1);
+			nokia_lcd_set_cursor(0, 20);
+			nokia_lcd_write_string("B-change code", 1);
+			nokia_lcd_set_cursor(0, 30);
+			nokia_lcd_write_string("C-check code", 1);
+			nokia_lcd_render();
+			break;
+			
+		case READ_KEY_WAIT:
+			break;	
 			
 		case READ_KEY_INIT:
 			if((USART_HasReceived(0))){
@@ -132,20 +183,37 @@ void Motor_Tick()
 					checker = 0;
 				}
 			}
-			
 				
 			nokia_lcd_clear();
-			nokia_lcd_set_cursor(10, 0);
-			nokia_lcd_write_string("PASSCODE?", 1);
-			
+			nokia_lcd_set_cursor(30, 0);
+			nokia_lcd_write_string("CODE?", 1);
 			nokia_lcd_set_cursor(10, 30);
+			
 			nokia_lcd_write_string("Entry: ", 1);
 			nokia_lcd_set_cursor(65, 30);
 			nokia_lcd_write_char(key, 1);
 			nokia_lcd_render();
 			break;
 			
-		case READ_KEY_WAIT:
+		case CURRENT_PASS:
+			nokia_lcd_clear();
+			nokia_lcd_set_cursor(5, 10);
+			nokia_lcd_write_string("Current Code:", 1);
+			nokia_lcd_set_cursor(10, 20);
+			nokia_lcd_write_char(password, 1);
+			nokia_lcd_set_cursor(20, 20);
+			nokia_lcd_write_char(password2, 1);
+			nokia_lcd_render();
+			break;
+			
+		case CHANGE_PASS:
+			nokia_lcd_clear();
+			nokia_lcd_set_cursor(5, 10);
+			nokia_lcd_write_string("New Code:", 1);
+			nokia_lcd_render();
+			break;
+			
+		case READ_KEY_WAIT_2:
 			break;
 			
 		case READ_KEY_INIT_2:
@@ -184,7 +252,7 @@ void Motor_Tick()
 			
 			
 		case READ_KEY:
-			if(((key == '1') && (key2 == '2')) || (bt_check)){ //LOCK = 1 2
+			if(((key == password) && (key2 == password2)) || (bt_check)){ //LOCK = 1 2
 				lr = 2;
 			}
 			else{
