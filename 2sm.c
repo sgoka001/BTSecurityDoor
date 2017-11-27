967ebe8dea5d6786e8b8844b7ccff12d20cca3df
Srisri Gokanapudy <sgoka001@ucr.edu>
2017-11-26 17:36:27 -0800

    fixed timing error

--- 2sm.c
+++ 2sm.c
@@ -42,13 +42,15 @@ unsigned char checker = 0; // 1 = valid entry, 0 = no key entered
 unsigned char checker2 = 0; // 1 = valid entry, 0 = no key entered
 unsigned char checker3 = 0; // 1 = valid entry, 0 = no key entered
 
-unsigned char passwordarray[3] = {'1', '2', '3'};
+unsigned char password = '1';
+unsigned char password2 = '2';
+unsigned char password3 = '3';
 
 unsigned char lr = 0; // unlock = 2
 unsigned char room = 0; // bed = 1 garage = 2
 //////////////////////////////////////////////////////////////////////// LOCAL VARIABLES ////////////////////////////////////////////////////////////////////////
 
-enum MotorState {READ_MOTION, MENU, READ_KEY_WAIT, READ_KEY_INIT, CURRENT_PASS, CHANGE_PASS, READ_KEY_WAIT_2, READ_KEY_INIT_2, READ_KEY_WAIT_3, READ_KEY_INIT_3, READ_KEY, LOCK, UNLOCK} motor_state;
+enum MotorState {READ_MOTION, MENU, READ_KEY_WAIT, READ_KEY_INIT, CURRENT_PASS, CHANGE_PASS, READ_KEY_WAIT_2, READ_KEY_INIT_2, READ_KEY_WAIT_3, READ_KEY_INIT_3, READ_KEY, CORRECT} motor_state;
 
 void Motor_Init(){
 	motor_state = READ_MOTION;
@@ -56,7 +58,6 @@ void Motor_Init(){
 
 void Motor_Tick()
 {
-	char garage_button = (GetBit(~PIND, 3)); //if button is pressed then = 8
 	char motion_sensor = (GetBit(~PIND, 2)); //if motion is sensed then = 4
 	switch(motor_state)
 	{	
@@ -73,61 +74,56 @@ void Motor_Tick()
 			break;
 			
 		case READ_KEY_WAIT:
-			if(cnt > 100){motor_state = READ_KEY_INIT; cnt = 0;}
+			if(cnt > 30){motor_state = READ_KEY_INIT; cnt = 0;}
 			else{motor_state = READ_KEY_WAIT; cnt = cnt + 1;}
 			break;
 			
 		case READ_KEY_INIT:
 			if((checker == 1) && (bt_check == 0)){motor_state = READ_KEY_WAIT_2;}
-			else if((checker == 0) && (bt_check == 1)){motor_state = UNLOCK; lr = 2;}
+			else if((checker == 0) && (bt_check == 1)){motor_state = CORRECT; lr = 2;}
 			else if((checker == 0) && (bt_check == 0)){motor_state = READ_KEY_INIT;}
 			break;
 			
 		case CURRENT_PASS:
-			if(cnt > 100){motor_state = MENU; cnt = 0;}
+			if(cnt > 30){motor_state = MENU; cnt = 0;}
 			else{motor_state = CURRENT_PASS; cnt = cnt + 1;}
 			break;
 			
 		case CHANGE_PASS:
-			if(cnt > 100){motor_state = MENU; cnt = 0;}
+			if(cnt > 30){motor_state = MENU; cnt = 0;}
 			else{motor_state = CHANGE_PASS; cnt = cnt + 1;}
 			break;
 			
 		case READ_KEY_WAIT_2:
-			if(cnt > 200){motor_state = READ_KEY_INIT_2; cnt = 0;}
+			if(cnt > 30){motor_state = READ_KEY_INIT_2; cnt = 0;}
 			else{motor_state = READ_KEY_WAIT_2; cnt = cnt + 1;}
 			break;
 			
 		case READ_KEY_INIT_2:
 			if((checker2 == 1) && (bt_check == 0)){motor_state = READ_KEY_WAIT_3;}
-			else if((checker2 == 0) && (bt_check == 1)){motor_state = UNLOCK; lr = 2;}
+			else if((checker2 == 0) && (bt_check == 1)){motor_state = CORRECT; lr = 2;}
 			else if((checker2 == 0) && (bt_check == 0)){motor_state = READ_KEY_INIT_2;}
 			break;
 			
 		case READ_KEY_WAIT_3:
-			if(cnt > 200){motor_state = READ_KEY_INIT_3; cnt = 0;}
+			if(cnt > 30){motor_state = READ_KEY_INIT_3; cnt = 0;}
 			else{motor_state = READ_KEY_WAIT_3; cnt = cnt + 1;}
 			break;
 			
 		case READ_KEY_INIT_3:
 			if((checker3 == 1) && (bt_check == 0)){motor_state = READ_KEY;}
-			else if((checker3 == 0) && (bt_check == 1)){motor_state = UNLOCK; lr = 2;}
+			else if((checker3 == 0) && (bt_check == 1)){motor_state = CORRECT; lr = 2;}
 			else if((checker3 == 0) && (bt_check == 0)){motor_state = READ_KEY_INIT_3;}
 			break;
 			
 		case READ_KEY:
-			if (lr == 2){motor_state = UNLOCK;}
+			if (lr == 2){motor_state = CORRECT; cnt = 0;}
 			else {motor_state = READ_KEY;}
 			break;
-		
-		case UNLOCK:
-			if(cnt > 200) {motor_state = LOCK; cnt = 0; motion_sensor = 0;}
-			else {motor_state = UNLOCK; cnt = cnt + 1;}
-			break;
 			
-		case LOCK:
-			if(cnt > 200) {motor_state = READ_MOTION; cnt = 0; lr = 0; motion_sensor = 0;}
-			else {motor_state = LOCK; cnt = cnt + 1;}
+		case CORRECT:
+			if(cnt > 200){motor_state = READ_MOTION; cnt = 0;}
+			else{motor_state = CORRECT; cnt = cnt + 1;}
 			break;
 		
 		default:
@@ -151,6 +147,7 @@ void Motor_Tick()
 			break;
 			
 		case MENU:
+			motion_sensor = 0;
 			menu_key = GetKeypadKey();
 			if ((menu_key == 'A')){
 				menu_check = 1;
@@ -210,11 +207,11 @@ void Motor_Tick()
 			nokia_lcd_set_cursor(5, 10);
 			nokia_lcd_write_string("Current Code:", 1);
 			nokia_lcd_set_cursor(10, 20);
-			nokia_lcd_write_char(passwordarray[0], 1);
+			nokia_lcd_write_char(password, 1);
 			nokia_lcd_set_cursor(20, 20);
-			nokia_lcd_write_char(passwordarray[1], 1);
+			nokia_lcd_write_char(password2, 1);
 			nokia_lcd_set_cursor(30, 20);
-			nokia_lcd_write_char(passwordarray[2], 1);
+			nokia_lcd_write_char(password3, 1);
 			nokia_lcd_render();
 			break;
 			
@@ -301,7 +298,7 @@ void Motor_Tick()
 			
 			
 		case READ_KEY:
-			if((key == passwordarray[0]) && (key2 == passwordarray[1]) && (key3 == passwordarray[2])){ //LOCK = 1 2
+			if((key == password) && (key2 == password2) && (key3 == password3)){ //LOCK = 1 2
 				lr = 2;
 			}
 			else{
@@ -323,16 +320,76 @@ void Motor_Tick()
 			
 			break;
 		
+		case CORRECT:
+			break;
+
+		default:
+			break;
+	}
+}
+
+void MotorTask()
+{
+	Motor_Init();
+   for(;;) 
+   { 	
+	Motor_Tick();
+	vTaskDelay(10); 
+   } 
+}
+
+void StartSecPulse(unsigned portBASE_TYPE Priority)
+{
+	xTaskCreate(MotorTask, (signed portCHAR *)"MotorTask", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+enum ControlState {WAIT, LOCK, UNLOCK} control_state;
+
+void Control_Init(){
+	control_state = WAIT;
+}
+
+void Control_Tick()
+{
+	char garage_button = (GetBit(~PIND, 3)); //if button is pressed then = 8
+	switch(control_state){
+		case WAIT:
+			if(lr = 2) {control_state = UNLOCK; cnt = 0;}
+			else {control_state = WAIT; cnt = cnt + 1;}
+		
+		case UNLOCK:
+			if(cnt > 200) {control_state = LOCK; cnt = 0;}
+			else {control_state = UNLOCK; cnt = cnt + 1;}
+			break;
+	
+		case LOCK:
+			if(cnt > 200) {control_state = WAIT; cnt = 0; lr = 0;}
+			else {control_state = LOCK; cnt = cnt + 1;}
+			break;
+	
+		default:
+			control_state = WAIT;
+			break;
+	}
+
+	switch(control_state){
+		case WAIT:
+			nokia_lcd_clear();
+			nokia_lcd_set_cursor(10, 10);
+			nokia_lcd_write_string("gucci!", 1);
+			nokia_lcd_render();
+			break;
+		
 		case UNLOCK:
 			if(lr){
 				if (temp < 7) {temp = temp + 1;}
 				else {temp = 0;}
-			
+					
 				if((numPhases - 1) == 0)
 				{
 					lr = 0;
 				}
-			} 
+			}
 			if(garage_button == 8){PORTA = (loc[temp] << 4); PORTD = ((0x01 << 4) | PORTD); room = 2;}
 			else{PORTA = loc[temp]; PORTD = SetBit(PORTD,4,0); room = 1;}
 				
@@ -359,21 +416,21 @@ void Motor_Tick()
 			nokia_lcd_write_char(room + '0', 1);
 			nokia_lcd_render();
 			break;
-			
+				
 		case LOCK:
 			if(lr){
 				if (temp > 0) {temp = temp - 1;}
 				else {temp = 7;}
-			
+					
 				if((numPhases - 1) == 0)
 				{
 					lr = 0;
 				}
-				
+					
 			}
 			if(garage_button == 8){PORTA = (loc[temp] << 4); PORTD = ((0x01 << 4) | PORTD); room = 2;}
 			else{PORTA = loc[temp]; PORTD = SetBit(PORTD,4,0); room = 1;}
-			
+				
 			nokia_lcd_clear();
 			nokia_lcd_set_cursor(10, 10);
 			nokia_lcd_write_string("LOCKING!", 1);
@@ -384,26 +441,26 @@ void Motor_Tick()
 			nokia_lcd_render();
 			break;
 		
-		default:
+		default:	
 			break;
 	}
 }
 
-void MotorTask()
+void ControlTask()
 {
-	Motor_Init();
-   for(;;) 
-   { 	
-	Motor_Tick();
-	vTaskDelay(3); 
-   } 
+	Control_Init();
+	for(;;)
+	{
+		Control_Tick();
+		vTaskDelay(3);
+	}
 }
 
-void StartSecPulse(unsigned portBASE_TYPE Priority)
+void ControlSecPulse(unsigned portBASE_TYPE Priority)
 {
-	xTaskCreate(MotorTask, (signed portCHAR *)"MotorTask", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
+	xTaskCreate(ControlTask, (signed portCHAR *)"ControlTask", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
 }
- 
+
 int main(void) 
 {
    DDRA = 0xFF; PORTA = 0x00; //stepper
@@ -417,6 +474,7 @@ int main(void)
    
    //Start Tasks  
    StartSecPulse(1);
+   ControlSecPulse(1);
     //RunSchedular 
    vTaskStartScheduler();
    /*
